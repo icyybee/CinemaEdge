7ff2e577e0eafa0526626a94198650a3be3e38f3
Beatrice Egumandi <ushiksmandi@gmail.com>
2023-01-27 17:05:02 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Home from "./routes/home/home.routes";
 import SignIn from "./routes/signIn/signIn.component";
 import HomePage from "./routes/homePage/homePage.component";
 
-import './App.scss';
-
 const App = () => {
   return (
     <Routes>
