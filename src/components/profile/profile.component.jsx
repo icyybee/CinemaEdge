13fe9a6e2a75fea38e7163ed1fce1ae1483a6946
Beatrice Egumandi <ushiksmandi@gmail.com>
2023-02-04 02:23:14 +0100

    intro and fix

--- src/components/profile/profile.component.jsx
+++ src/components/profile/profile.component.jsx
@@ -1,8 +1,10 @@
+import './profile.styles.scss';
+
 const Profile = ({image, nickname}) => {
     return (
         <div className="profile__body">
             <div className='profile__body--img'>
-                <img src={image} alt={nickname} />
+                <img src={image} alt={nickname}/>
             </div>
             <div className='profile__body--text'>
                 {nickname}
